c86bc560e83d8f7b9e1e299f94f719e5350c52db
ahm-muzahid <ahm.muzahid@gmail.com>
2023-08-15 17:12:54 +0600

    project upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home';
+import Phone from './components/Phone';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+          loader: () => fetch('http://localhost:5000/phones')
+        },
+        {
+          path: '/phones/:id',
+          element: <Phone></Phone>
+        }
+      ]
+    }
+  ])
+  return (
+    <div className="App">
+    <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
